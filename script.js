42b9db26247e02a886285aa17ae51e18820f1613
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-23 16:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+    // Stops the page from reloading
+    event.preventDefault();
+    
+    // Corrected typos in the next two lines
+    let form = document.getElementById("MyForm");
+    let radius = form["radius"].value;
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    // Corrected typos in the next two lines
+    let volume = document.getElementById("volume");
+    form["volume"].value = (4/3) * Math.PI * radius * radius * radius;
+}
+
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+};
